c2e2eef5c02fac7c474afa310be5124194aa97c2
Mikael Pettersson <mikael.pettersson201@gmail.com>
2015-02-19 11:18:29 +0100

    Restarted project and added AR

---
+++ TruckAR/app/src/main/java/masterthesis/truckar/MainActivity.java
@@ -0,0 +1,62 @@
+package masterthesis.truckar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.jni.IGeometry;
+import com.metaio.sdk.jni.IMetaioSDKCallback;
+
+
+public class MainActivity extends ARViewActivity {
+
+    @Override
+    protected int getGUILayout() {
+        return 0;
+    }
+
+    @Override
+    protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
+        return null;
+    }
+
+    @Override
+    protected void loadContents() {
+
+    }
+
+    @Override
+    protected void onGeometryTouched(IGeometry geometry) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
