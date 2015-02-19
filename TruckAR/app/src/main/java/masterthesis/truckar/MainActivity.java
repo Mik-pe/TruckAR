d5d5a84b0e4e7e7104f93112200b1d68ed44e9a2
Mikael Pettersson <mikael.pettersson201@gmail.com>
2015-02-19 14:22:40 +0100

    Added template

--- TruckAR/app/src/main/java/masterthesis/truckar/MainActivity.java
+++ TruckAR/app/src/main/java/masterthesis/truckar/MainActivity.java
@@ -1,62 +1,93 @@
+// Copyright 2007-2014 Metaio GmbH. All rights reserved.
 package masterthesis.truckar;
 
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
 
-import com.metaio.sdk.ARViewActivity;
-import com.metaio.sdk.jni.IGeometry;
-import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.tools.io.AssetsManager;
 
+public class MainActivity extends Activity
+{
 
-public class MainActivity extends ARViewActivity {
-
-    @Override
-    protected int getGUILayout() {
-        return 0;
-    }
-
-    @Override
-    protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
-        return null;
-    }
+    /**
+     * Task that will extract all the assets
+     */
+    private AssetsExtracter mTask;
 
     @Override
-    protected void loadContents() {
-
-    }
-
-    @Override
-    protected void onGeometryTouched(IGeometry geometry) {
-
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // Enable metaio SDK debug log messages based on build configuration
+        MetaioDebug.enableLogging(BuildConfig.DEBUG);
+
+        // extract all the assets
+        mTask = new AssetsExtracter();
+        mTask.execute(0);
     }
 
+    /**
+     * This task extracts all the application assets to an external or internal location
+     * to make them accessible to Metaio SDK
+     */
+    private class AssetsExtracter extends AsyncTask<Integer, Integer, Boolean>
+    {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        protected void onPreExecute()
+        {
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        protected Boolean doInBackground(Integer... params)
+        {
+            try
+            {
+                // Extract all assets and overwrite existing files if debug build
+                AssetsManager.extractAllAssets(getApplicationContext(), BuildConfig.DEBUG);
+            }
+            catch (IOException e)
+            {
+                MetaioDebug.log(Log.ERROR, "Error extracting assets: "+e.getMessage());
+                MetaioDebug.printStackTrace(Log.ERROR, e);
+                return false;
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(Boolean result)
+        {
+            if (result)
+            {
+                // Start AR Activity on success
+                Intent intent = new Intent(getApplicationContext(), TruckAR.class);
+                startActivity(intent);
+            }
+            else
+            {
+                // Show a toast with an error message
+                Toast toast = Toast.makeText(getApplicationContext(), "Error extracting application assets!", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.show();
+            }
+
+            finish();
+        }
+
     }
+
 }
+
